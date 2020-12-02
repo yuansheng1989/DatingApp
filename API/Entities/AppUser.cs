3c9dda09f9ed60da56c0ba1cc45e5a19f5595935
yuansheng1989 <yn432228@dal.ca>
2020-12-02 10:55:42 -0500

    add properties for password in AppUser

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,5 +9,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PassowordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
