35fe6c48d8b91c392c0812c5763c7eac63aaf718
yuansheng1989 <yn432228@dal.ca>
2021-01-10 15:12:20 -0500

    add photo upload functionality

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -52,7 +52,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
             if (user == null)
             {
                 return Unauthorized("Invalid username");
@@ -72,7 +72,8 @@ namespace API.Controllers
             return new UserDto
             {
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
